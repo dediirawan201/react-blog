8905164941843cc896b9e597e3463756c6ee3a21
dedi <dediirwansyah1925@gmail.com>
2021-10-27 11:19:14 +0700

    membuat react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import {Topbar} from './components/molecouls'
 import {Home, Login, Settings, Single, Write} from './pages';
 import Register from './pages/Register';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
+  const user = false;
   return (
-    <>
+    <Router>
       <Topbar/>
-      <Register/>
-    </>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route path="/register">
+          {user ? <Home/> : <Register/>}
+        </Route>
+        <Route path="/login">
+        {user ? <Home/> : <Login/>}
+        </Route>
+        <Route path="/settings">
+        {user ? <Settings/> : <Register/>}
+        </Route>
+        <Route path="/write">
+        {user ? <Write/> : <Register/>}
+        </Route>
+        <Route path="/post/:postId">
+          <Single/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/molecouls/Topbar/index.js
+++ src/components/molecouls/Topbar/index.js
@@ -1,6 +1,8 @@
+import { Link } from 'react-router-dom';
 import './topbar.css';
 
-const Topbar = () => { 
+const Topbar = () => {
+    const user = false;
     return (
         <div className="top">
             <div className="topLeft">
@@ -8,23 +10,44 @@ const Topbar = () => {
                 <i className="topIcon fab fa-twitter-square"></i>
                 <i className="topIcon fab fa-pinterest-square"></i>
                 <i className="topIcon fab fa-instagram-square"></i>
-                </div>
+            </div>
             <div className="topCenter">
                 <div className="topList">
-                    <li className=" topListItem">HOME</li>
-                    <li className=" topListItem">ABOUT</li>
-                    <li className=" topListItem">CONTACT</li>
-                    <li className=" topListItem">WRITE</li>
-                    <li className=" topListItem">LOGOUT</li>
+                    <li className=" topListItem">
+                        <Link className="link" to="/">HOME</Link></li>
+                    <li className=" topListItem">
+                        <Link className="link" to="/">ABOUT</Link>
+                    </li>
+                    <li className=" topListItem">
+                        <Link className="link" to="/">CONTACT</Link>
+                    </li>
+                    <li className=" topListItem">
+                        <Link className="link" to="/">WRITE</Link>
+                    </li>
+                    <li className=" topListItem">
+                        {user && "LOGOUT"}
+                    </li>
                 </div>
             </div>
             <div className="topRight">
-                <img
-                className="topImg"
-                src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" 
-                alt=""
-                />
-                <i className="topSearchIcon fas fa-search"></i> 
+                {user ?
+                    (
+                        <img
+                            className="topImg"
+                            src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                            alt=""
+                        />
+                    ) : (
+                        <ul className="topList">
+                            <li className="topListItem">
+                                <Link className="link" to="/login">LOGIN</Link>
+                            </li>
+                            <li className="topListItem">
+                                <Link className="link" to="/register">REGISTER</Link>
+                            </li>
+                        </ul>
+                    )}
+                <i className="topSearchIcon fas fa-search"></i>
             </div>
         </div>
     )
